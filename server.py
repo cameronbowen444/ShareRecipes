7e0d4b600d2135b64ef2f9d1702c142ad8d693dc
cameronbowen444 <cameronbowen555@gmail.com>
2026-06-22 14:36:09 -0700

    server fixed typo

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from flask_app import app
 from flask_app.controllers import users, recipes
+import os
 
-
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
